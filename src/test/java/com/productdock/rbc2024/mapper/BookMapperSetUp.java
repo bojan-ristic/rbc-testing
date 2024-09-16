a39df697feb120ad251e938de85c4d2f60822ef7
Bojan Ristic <bojan.ristic.ext@adevinta.com>
2024-09-16 14:10:04 +0200

    Add unit test for convertModelToBookDetailsDto

---
+++ src/test/java/com/productdock/rbc2024/mapper/BookMapperSetUp.java
@@ -0,0 +1,29 @@
+package com.productdock.rbc2024.mapper;
+
+import com.productdock.rbc2024.dto.BookDetailsDto;
+import com.productdock.rbc2024.model.Book;
+
+class BookMapperSetUp {
+    static final long BOOK_ID = 1L;
+    static final String BOOK_NEW_AUTHOR = "author";
+    static final String BOOK_TITLE = "title";
+    static final int BOOK_NUMBER_OF_PAGES = 120;
+
+    static Book createBook() {
+        return Book.builder()
+                .id(BOOK_ID)
+                .author(BOOK_NEW_AUTHOR)
+                .title(BOOK_TITLE)
+                .numberOfPages(BOOK_NUMBER_OF_PAGES)
+                .build();
+    }
+
+    static BookDetailsDto createExpectedResultBookDetailsDto() {
+        return BookDetailsDto.builder()
+                .id(BOOK_ID)
+                .author(BOOK_NEW_AUTHOR)
+                .title(BOOK_TITLE)
+                .numberOfPages(BOOK_NUMBER_OF_PAGES)
+                .build();
+    }
+}
--- src/test/java/com/productdock/rbc2024/mapper/BookMapperShould.java
+++ src/test/java/com/productdock/rbc2024/mapper/BookMapperShould.java
@@ -1,48 +1,31 @@
 package com.productdock.rbc2024.mapper;
 
-import com.productdock.rbc2024.dto.BookDetailsDto;
-import com.productdock.rbc2024.model.Book;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@Slf4j
 class BookMapperShould {
 
     private BookMapper bookMapper;
 
-    @BeforeAll
-    static void setup() {
-        log.info("BeforeAll");
-    }
-
-    @AfterAll
-    static void tearDown() {
-        log.info("AfterAll");
-    }
 
     @BeforeEach
     void setUp() {
         bookMapper = new BookMapper();
-        log.info("BeforeEach");
-    }
-
-    @AfterEach
-    void teardown() {
-        log.info("AfterEach");
     }
 
     @Test
     void convertModelToBookDetailsDto() {
-        var book = new Book();
+        // given
+        var book = BookMapperSetUp.createBook();
 
-        var actual = bookMapper.convertModelToBookDetailsDto(book);
+        // when
+        var bookDetailsDto = bookMapper.convertModelToBookDetailsDto(book);
 
-        var expected = new BookDetailsDto();
-        assertNotNull(actual);
-        assertEquals(expected, actual);
+        // then
+        var expected = BookMapperSetUp.createExpectedResultBookDetailsDto();
+        assertEquals(expected, bookDetailsDto);
     }
 
 }
