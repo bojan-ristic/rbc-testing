9cd1bb9fa20993e962b6c1f2a99ce22957733887
Bojan Ristic <bojan.ristic.ext@adevinta.com>
2024-09-16 14:33:53 +0200

    Crate method createComments

--- src/test/java/com/productdock/rbc2024/mapper/bookmapper/BookMapperSetUp.java
+++ src/test/java/com/productdock/rbc2024/mapper/bookmapper/BookMapperSetUp.java
@@ -2,6 +2,9 @@ package com.productdock.rbc2024.mapper.bookmapper;
 
 import com.productdock.rbc2024.dto.BookDetailsDto;
 import com.productdock.rbc2024.model.Book;
+import com.productdock.rbc2024.model.Comment;
+
+import java.util.List;
 
 class BookMapperSetUp {
     static final long BOOK_ID = 1L;
@@ -9,12 +12,32 @@ class BookMapperSetUp {
     static final String BOOK_TITLE = "title";
     static final int BOOK_NUMBER_OF_PAGES = 120;
 
+    static final long FIRST_COMMENT_ID = 1L;
+    static final String FIRST_COMMENT_CONTENT = "comment_content";
+
+    static final long SECOND_COMMENT_ID = 2L;
+    static final String SECOND_COMMENT_CONTENT = "comment_content";
+
     static Book createBook() {
         return Book.builder()
                 .id(BOOK_ID)
                 .author(BOOK_NEW_AUTHOR)
                 .title(BOOK_TITLE)
                 .numberOfPages(BOOK_NUMBER_OF_PAGES)
+                .build();
+    }
+
+    private static List<Comment> createComments() {
+        var theFirstComment = createComment(FIRST_COMMENT_ID, FIRST_COMMENT_CONTENT, null);
+        var theSecondComment = createComment(SECOND_COMMENT_ID, SECOND_COMMENT_CONTENT, null);
+        return List.of(theFirstComment, theSecondComment);
+    }
+
+    private static Comment createComment(Long id, String content, Book book) {
+        return Comment.builder()
+                .id(id)
+                .content(content)
+                .book(book)
                 .build();
     }
 
